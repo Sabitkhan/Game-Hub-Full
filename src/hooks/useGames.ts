7ff4a7b3166a5a5424d3becc1b9089b46ed02235
Sabit khan <mesabitkhan@gmail.com>
2023-07-22 10:44:51 +0500

    implement infinite scrole

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,4 +1,4 @@
-import { useInfiniteQuery} from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
 import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
@@ -15,19 +15,20 @@ export interface Game {
 const useGames = (gameQuery: GameQuery) =>
   useInfiniteQuery<FetchResponse<Game>, Error>({
     queryKey: ["games", gameQuery],
-    queryFn: ({pageParam = 1}) =>
+    queryFn: ({ pageParam = 1 }) =>
       apiClient.getAll({
         params: {
           genres: gameQuery.genre?.id,
           parents_platforms: gameQuery.platform?.id,
           ordering: gameQuery.sortOrder,
           search: gameQuery.searchText,
-          page:pageParam
+          page: pageParam,
         },
       }),
-      getNextPageParam:(lastPage,allPages)=>{
-        return allPages.length ? allPages.length + 1: undefined;
-      }
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage.next ? allPages.length + 1 : undefined;
+      },
+      staleTime: 24 * 60 * 60 * 1000,
   });
 
 export default useGames;
