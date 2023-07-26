0f9b6c65e199b6d6e12513ed9301f0d7e9d46f73
Sabit khan <mesabitkhan@gmail.com>
2023-07-26 20:14:35 +0500

    Refactor - code with zustanf

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,21 +1,19 @@
 import { HStack } from "@chakra-ui/layout";
-import { Image} from "@chakra-ui/react";
+import { Image } from "@chakra-ui/react";
 import Logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-interface Props {
-  onSearch: (searchText: string) => void;
-}
-const NavBar = ({onSearch}:Props) => {
+
+const NavBar = () => {
   return (
     <div>
-      <HStack  padding='10px'>
+      <HStack padding="10px">
         <Image src={Logo} boxSize={59} />
-        <SearchInput onSearch={onSearch}/>
+        <SearchInput />
         <ColorModeSwitch />
       </HStack>
     </div>
-  ); 
+  );
 };
 
 export default NavBar;
